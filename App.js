53eb16ad6b68cda17b58d730863e8992b1c38831
Rahullll <rahullakkavarapu22@gmail.com>
2023-07-28 15:29:46 +0530

    JSX and Functional Components

--- App.js
+++ App.js
@@ -1,11 +1,27 @@
 import React from "react";
 import ReactDOM  from "react-dom/client";
 
-
+//React.createElement ==> Object =>Becomes HTML Element upon rendering to DOM
 const heading = React.createElement("h1",
-{id:"heading", xyz:"abc"}  ,
+{id: "headdding"},
 "Hello World from React!!");
 
+console.log(heading);
+
+//JSX - JSX is HTML like syntax
+const JSXheading = <h1 id="headingg">Jsx Heading</h1>
+console.log(JSXheading);
+
+const HeadingCompoponent = ()=>{
+    return <h1>Hello Component1</h1>
+}
+const HeadingCompoponent2 = ()=>(
+    <>    
+    <HeadingCompoponent/>
+    <h1>Hello Component2</h1> 
+    </>
+
+    )
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(heading);
+root.render(<HeadingCompoponent2/>);
