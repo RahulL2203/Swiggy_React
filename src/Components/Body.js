418cc2076dbb76c2b784ab2953a73cad294e6ed3
Rahullll <rahullakkavarapu22@gmail.com>
2023-07-30 11:19:33 +0530

    Routing , RestaurantMenu display

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -1,6 +1,8 @@
 import RestaurantCard from "./RestaurantCard";
 import { useState ,useEffect} from "react";
 import Shimmer from "./Shimmer";
+import { Link } from "react-router-dom";
+
 
 
 const Body =()=>{
@@ -61,7 +63,9 @@ const Body =()=>{
 
             <div className="restaurant-container">
                 {filteredRestaurants?.map((restaurant)=>(
-                    <RestaurantCard key ={restaurant?.info?.id} resData={restaurant}/>
+                    <Link to ={"/restaurants/"+ restaurant?.info?.id}>
+                        <RestaurantCard key ={restaurant?.info?.id} resData={restaurant}/>
+                    </Link>
                 ))
                  }
 
--- src/Components/RestaurantMenu.js
+++ src/Components/RestaurantMenu.js
@@ -1,37 +1,53 @@
 import { useEffect ,useState} from "react";
 import Shimmer from "./Shimmer";
+import { useParams } from "react-router-dom";
+import { MENU_URL } from "../utils/constants";
 const RestaurantMenu = ()=>{
     const [resInfo,setResInfo] = useState(null); 
+    const [menuItems1,setMenuItems1] = useState([]); 
+
+    const {resId} = useParams();
     useEffect(()=>{
         fetchMenu()
     },[])
 
     const fetchMenu = async ()=>{
-            const data = await fetch("https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=17.5002987&lng=78.396665&restaurantId=1691&catalog_qa=undefined&submitAction=ENTER");
+            const data = await fetch(MENU_URL  + resId);
             const json = await data.json();
-            console.log(json);
+            console.log(json)
             setResInfo(json.data)
+            setMenuItems1(json.data.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[3]?.card?.card?.categories);
+            
     };
 
-    // const {name, cuisines, costForTwoMessage}= resInfo?.cards[0]?.card?.card?.info
+    if(resInfo==null) return <Shimmer/>;
 
-    const menuItems =resInfo.cards[2].groupedCard.cardGroupMap.REGULAR.cards[1].card.card.carousel
+   const {name, cuisines, costForTwoMessage} = resInfo?.cards[0]?.card?.card?.info
+
+    console.log(menuItems1 );
+
+
     return (
         <div className="menu ">
-            {/* <h1>{name} </h1>
-            <p>{cuisines.join(", ")} - {costForTwoMessage}</p> */}
-          
+            <h1>{name} </h1>
+            <p>{cuisines.join(", ")} - {costForTwoMessage}</p>
             <ul>
-                {()=>{
-                    menuItems.map((item)=>{ item.title
-                       
-                    }
-                    )
-                }} 
-                <li>Biryani</li>
-                <li>Chineese</li>
-                <li>IceCreams</li>
+            { menuItems1.map((item,i)=>
+            (<div key ={i}><h3>{item?.title} </h3>
+            <ul>{item.itemCards.map((sub)=>
+            <li key={sub.card.info.id}>{sub.card.info.name} - Rs.{sub.card.info.price/100}</li>)}
+               
             </ul>
+            
+            </div>))}
+            </ul>
+          {/* [0].itemCards[0].card.info
+
+          [0].itemCards[4].card.info */}
+
+{/* {menuItems1[0].map((item,index)=><li key={index}>{item.itemCards.card.info.name }</li>)} */}
+          
+        
         </div>
     )
 }
